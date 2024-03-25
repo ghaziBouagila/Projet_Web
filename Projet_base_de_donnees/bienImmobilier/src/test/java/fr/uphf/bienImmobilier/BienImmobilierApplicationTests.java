ec62f0a44204ecd629f6ac8ff1fd45a12f85937b
Moustapha-BA <135900203+Moustapha-BA@users.noreply.github.com>
2024-03-25 14:56:10 +0100

    Add files via upload

---
+++ Projet_base_de_donnees/bienImmobilier/src/test/java/fr/uphf/bienImmobilier/BienImmobilierApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uphf.bienImmobilier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienImmobilierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
